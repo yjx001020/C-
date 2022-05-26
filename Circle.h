63aa3561e1d98ea53fc7373bba0d36512a425c48
Jessie Yang <jiaxiyan@usc.edu>
2022-05-25 20:38:45 -0700

    Update Circle.h

--- Circle.h
+++ Circle.h
@@ -1,6 +1,8 @@
 #pragma once
+//abstract class: has pure virtual function = 0. cannot create instance
+//make base class destructor~ virtual
 
-class Circle {
+class Circle: public Shape {
   private:
     double r;
 
